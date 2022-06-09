517cc291121fd10025760cf99d8061993771f3c5
Gabriel Rodríguez Domínguez <gatrixrd2@gmail.com>
2022-06-09 09:56:38 -0500

    Update instalaSESEAP_S1.sh

--- instalaSESEAP_S1.sh
+++ instalaSESEAP_S1.sh
@@ -165,7 +165,7 @@
 								if [ $executarMontado -gt 0 ];
 									then
 												# E S T A B L E C I E N D O    V A L O R E S   E N   E L   A P P . S E T T I N G S    D E L    P R O Y E C T O    . N E T
-												find ~ -name appsettings.json | xargs sed -i "s/apiName/$apiName/g" appsettings.json
+												find ~ -name "API.S1.SESEAP" | xargs sed -i "s/apiName/$apiName/g" appsettings.json
 												sed -i "s/clientId/$clientId/g" appsettings.json
 												sed -i "s/clientScopeRead/$clientScopeReadaux/g" appsettings.json
 												sed -i "s/clientScopeWrite/$clientScopeWriteaux/g" appsettings.json
