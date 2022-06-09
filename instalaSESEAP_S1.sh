c23a5f66344ed6a5ef19189355b29389e0f80869
Gabriel Rodríguez Domínguez <gatrixrd2@gmail.com>
2022-06-09 09:52:28 -0500

    Update instalaSESEAP_S1.sh

--- instalaSESEAP_S1.sh
+++ instalaSESEAP_S1.sh
@@ -18,6 +18,7 @@
 				echo -e "\033[33mEliminando archivo .zip\033[0m"
 				rm  PDEPuebla.S1.PDN.zip
 				clear
+				cd ..
 			}
 
 
@@ -126,7 +127,7 @@
 										tempmongoHostname=$mongoHostname
 										mongoHostname=$mongoHostnameaux
 										cd ..
-										find / -name parametrosConfiguracion.txt -type f -print | xargs sed -i "s/"${tempmongoHostname}"/$mongoHostnameaux/g" parametrosConfiguracion.txt
+										find ~ -name parametrosConfiguracion.txt | xargs sed -i "s/"${tempmongoHostname}"/$mongoHostnameaux/g" parametrosConfiguracion.txt
 										cd API.S1.SESEAP
 										executarMontado=1
 								fi
@@ -164,7 +165,7 @@
 								if [ $executarMontado -gt 0 ];
 									then
 												# E S T A B L E C I E N D O    V A L O R E S   E N   E L   A P P . S E T T I N G S    D E L    P R O Y E C T O    . N E T
-												find / -name appsettings.json -type f -print | xargs sed -i "s/apiName/$apiName/g" appsettings.json
+												sed -i "s/apiName/$apiName/g" appsettings.json
 												sed -i "s/clientId/$clientId/g" appsettings.json
 												sed -i "s/clientScopeRead/$clientScopeReadaux/g" appsettings.json
 												sed -i "s/clientScopeWrite/$clientScopeWriteaux/g" appsettings.json
