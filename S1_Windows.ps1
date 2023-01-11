9b7dc1ff0f805dd11edf3d04452e7f5705cf18dc
Gabriel Rodríguez Domínguez <52460428+gatroxrd@users.noreply.github.com>
2023-01-11 16:51:11 -0600

    Update S1_Windows.ps1
    Forzando detener Contenedor e Imagen del dotnet

--- S1_Windows.ps1
+++ S1_Windows.ps1
@@ -1,5 +1,14 @@
 #!/bin/bash
     Set-Location c:\DeclaracionesPDN\API.SESEAP\USTPD.S1
+	try
+	{
+		docker container stop $(docker container ls -q --filter name=dotnet*) | xargs docker rm
+		docker rmi $(docker images --format "{{.Repository}}:{{.Tag}}"|findstr "dotnet")
+	}
+	catch
+	{
+		
+	}
     try
     {
         docker image rm dotnet@latest -f
